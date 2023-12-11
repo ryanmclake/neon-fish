5ced8a880ac1f9fef11d29c2d4f8d254d713eb64
ryanmclake <rmcclure@carnegiescience.edu>
2023-12-11 15:05:47 -0700

    update some stuff

--- download/download_fish_data.R
+++ download/download_fish_data.R
@@ -5,17 +5,17 @@ gc()
 # install packages needed for the analysis
 
 if (!"pacman" %in% installed.packages()) install.packages("pacman")
-pacman::p_load(neonstore)
+pacman::p_load(neonstore, restR2)
 
 # quickly ingest the fish data across all sites using the neonstore package
 # more efficient than neonUtilities as it avoids repeat downloading
 
 # this finds the existing the directory
-neonstore::neon_dir()
+#neonstore::neon_dir()
 
 # This sets the new directory on your local machine that the data are loaded to
-Sys.setenv("NEONSTORE_HOME" = "C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/neon-fish/neonstore")
-Sys.setenv("NEONSTORE_DB" = "C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/neon-fish/neonstore")
+#Sys.setenv("NEONSTORE_HOME" = "C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/neon-fish/neonstore")
+#Sys.setenv("NEONSTORE_DB" = "C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/neon-fish/neonstore")
 
 # download the neon-fish data
 neonstore::neon_download(product="DP1.20107.001")
--- visualize/visualize_fish_data.R
+++ visualize/visualize_fish_data.R
@@ -5,7 +5,7 @@ gc()
 # install packages needed for the analysis
 
 if (!"pacman" %in% installed.packages()) install.packages("pacman")
-pacman::p_load(tidyverse, neonstore)
+pacman::p_load(tidyverse, neonstore, lubridate, zoo)
 
 # generate a data frame of all tables that were downloaded
 table_index <- neonstore::neon_index(product="DP1.20107.001")
@@ -14,15 +14,25 @@ table_index <- neonstore::neon_index(product="DP1.20107.001")
 table_names <- unique(table_index$table) %>% str_subset(pattern = "-basic")
 
 # Identify the lakes
-lakes <- c("CRAM","LIRO","PRLO","PRLA","SUGG","BARC","TOOK")
+lakes <- c("CRAM","LIRO","PRPO","PRLA","SUGG","BARC","TOOK")
 
 # Identify the streams
 streams <- c()
 
 
 lake_fish_bulkCount <- neonstore::neon_read(table = table_names[1], site = lakes) %>%
-  dplyr::filter(siteID == "PRLA") %>%
-  dplyr::filter(str_detect(eventID, "fyke"))
+  dplyr::filter(siteID == c("PRLA","PRPO")) %>%
+  dplyr::filter(str_detect(eventID, "fyke")) %>%
+  dplyr::filter(taxonID == "PIMPRO") %>%
+  mutate(date = lubridate::date(passEndTime)) %>%
+  mutate(date = as.Date(as.yearmon(date, "%m/%Y"))) %>%
+  group_by(namedLocation) %>%
+  mutate(location_count = length(namedLocation))
+  
+
+
+
+
 
 ggplot(lake_fish_bulkCount, aes(passEndTime, bulkFishCount, color = as.character(passNumber)))+
   geom_point(size = 3)
